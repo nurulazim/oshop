ec9345cfb2888d5dbec25431be9c60c0d3cb1ecc
nurulazim <rizvisust2013@gmail.com>
2019-08-07 23:51:17 +0600

    login logout

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AngularFireAuth} from 'angularfire2/auth';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-bs-navbar',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bs-navbar.component.css']
 })
 export class BsNavbarComponent implements OnInit {
+  user: firebase.User;
 
-  constructor() { }
+  constructor(private  afAuth: AngularFireAuth) {
+    afAuth.authState.subscribe(user => this.user = user);
+  }
 
   ngOnInit() {
+  }
+  logout() {
+    this.afAuth.auth.signOut();
   }
 
 }
